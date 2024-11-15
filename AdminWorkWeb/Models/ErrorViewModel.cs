53eef9fcd7e123e1f32561757f2b1103e0b19a71
Юра <@DESKTOP-NALH133>
2024-11-15 21:52:46 +0200

    MVC

---
+++ AdminWorkWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AdminWorkWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
